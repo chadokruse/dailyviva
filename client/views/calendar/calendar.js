475c991e2af91f9ab06f81ee366d5c791488e95d
Chad Kruse <chadokruse@gmail.com>
2015-01-31 22:42:40 -0600

    Finish boilerplate + initial checkin for UI

---
+++ client/views/calendar/calendar.js
@@ -0,0 +1,8 @@
+Template.calendar.rendered = function () {
+  $('#calendar').datepicker({
+    //todayBtn: "linked",
+    multidate: true,
+    keyboardNavigation: false,
+    todayHighlight: true,
+  });
+};
