f9a2ceb3d9fbbadc3950432b4d87dd21d02b3eda
Chad Kruse <chadokruse@gmail.com>
2015-02-01 21:32:04 -0600

    Method fix

--- server/methods.js
+++ server/methods.js
@@ -8,7 +8,7 @@ Meteor.methods({
   removeDay: function (id, deselectedId) {
     console.log('Method called: addDay');
     console.log(id);
-    console.log(day);
+    console.log(deselectedId);
     Routines.update({ _id: id }, {$pull: { days: deselectedId }});
   }
 });
