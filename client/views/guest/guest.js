af69efd890b794a28331e2d154baff2219cb6275
Chad Kruse <chadokruse@gmail.com>
2015-02-03 17:42:29 -0600

    Persist new dates

--- client/views/guest/guest.js
+++ client/views/guest/guest.js
@@ -1,69 +1,45 @@
 Template.guest.rendered = function () {
   $('[data-toggle="tooltip"]').tooltip();
+
+  //Initialize calendar
   $('#calendar').datepicker({
-    //todayBtn: "linked",
     multidate: true,
     keyboardNavigation: false,
     todayHighlight: true,
   });
-  Session.set('test', true);
 
+  //Load current routine dates
+    var defaultId = '1';
+    Session.set('routineId', defaultId);
+    var id = Session.get('routineId');
+    var dates = Session.get('routine-' + id);
+    $('#calendar').datepicker('setDates', dates);
 
-  //Session.set('days', Routines.findOne({_id: Session.get('routineId')}).days);
-
+  //Capture date selections when calendar clicked - uses plugin event 'changeDate'
   $('#calendar').on('changeDate', function (e){
-      //var id = Session.get('routineId');
-      var day = e.date; //"latest date picked"
-      var countBefore = Session.get('count');
-      var countAfter = $('#calendar').datepicker('getDates').length;
-      console.log('CountBefore: ' + countBefore);
-      console.log('CountAfter: ' + countAfter);
-      //var q = $('#calendar').data().datepicker;
-      var viewDate = $('#calendar').data().datepicker.viewDate;
-
-      var date = viewDate;
-      var targetTime = new Date(date);
-      var timeZoneFromDB = -7.00; //time zone value from database
-      var tzDifference = targetTime.getTimezoneOffset();
-      var deselectedId = new Date(targetTime.getTime() + tzDifference * 60 * 1000);
-
-      console.log('viewDate: ' + deselectedId);
-      console.log('e.date: ' + e.date);
-      //alert("myObject is " + q.toSource());
-      //console.log('Valueof: ' + e.date.valueOf());;
-      //if ($.inArray(day, e.dates) != -1) {
-      if (countAfter < countBefore) {
-        console.log('Action: REMOVE');
-        //Meteor.call('removeDay', id, deselectedId, function (error, result) {});
-      } else {
-        console.log('Action: ADD');
-        //Meteor.call('addDay', id, deselectedId, function (error, result) {});
-      }
-      Session.set('count', countAfter);
-      return false;
+      var id = Session.get('routineId');
+      var fetchDates = $('#calendar').datepicker('getDates');
+      Session.set('routine-' + id, fetchDates);
+    //}
   });
 };
 
 Template.guest.events({
-  /*
-  'click .js-routine-select': function (e) {
-    //e.preventDefault();
-    var id = $(e.target).data('id');
-    var input = $(e.target).children(':radio').attr('checked', true);
-    //console.log('Routine btn clicked. Id: ' + id);
-    //Router.go('calendar', {view: 'monthly', id: id});
-  },
-  */
   'click .js-routine-select': function (e) {
     e.preventDefault();
-    //$('[contenteditable=true]').attr('contenteditable', false);
+    //Set the current routine
+    var newId = $(e.target).data('id');
+    Session.set('routineId', newId);
+    //If clicking an active routine, allow editing of text
     if ($(e.target).hasClass('active')) {
       $(e.target).attr('contenteditable','true');
       $(e.target).focus().select();
       $('[contenteditable=true]').not($(e.target)).attr('contenteditable', false);
+    } else {
+      //Fetch new dates and load them
+      var newDates = Session.get('routine-' + newId);
+      var loadDates = $('#calendar').datepicker('setDates', newDates);
     }
-    var id = $(e.target).data('id');
-    Session.set('routineId', id);
   },
   'keydown [contenteditable]': function (e) {
     if (e.keyCode === 13) {
@@ -71,31 +47,6 @@ Template.guest.events({
       return false;
     }
   },
-  'mouseenter .js-routine-select': function (e) {
-    //if ($(e.target).attr('contenteditable', true))
-      //$(e.target).tooltip('show');
-  },
-  'mouseleave [contenteditable]': function (e) {
-
-    //$(e.target).tooltip('hide');
-  },
-  /*
-  window.onload = function() {
-            var div = document.getElementById('editable');
-            div.onclick = function(e) {
-                this.contentEditable = true;
-                this.focus();
-                this.style.backgroundColor = '#E0E0E0';
-                this.style.border = '1px dotted black';
-            }
-
-            div.onmouseout = function() {
-                this.style.backgroundColor = '#ffffff';
-                this.style.border = '';
-                this.contentEditable = false;
-            }
-        }
-        */
   'click .js-add': function (e) {
     swal({
       title: 'Easy Tiger!',
@@ -105,9 +56,6 @@ Template.guest.events({
       confirmButtonClass: 'btn-blue-dark',
       confirmButtonText: 'OK',
       closeOnConfirm: true
-    },
-    function(){
-      //swal("Deleted!", "Your imaginary file has been deleted.", "success");
     });
   },
 
