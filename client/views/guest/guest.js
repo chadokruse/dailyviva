a14a145aae3ac98318f0ee36313a53e0312caa25
Chad Kruse <chadokruse@gmail.com>
2015-02-03 15:30:47 -0600

    Allow inline edit of button text

--- client/views/guest/guest.js
+++ client/views/guest/guest.js
@@ -6,6 +6,7 @@ Template.guest.rendered = function () {
     keyboardNavigation: false,
     todayHighlight: true,
   });
+  Session.set('test', true);
 
 
   //Session.set('days', Routines.findOne({_id: Session.get('routineId')}).days);
@@ -44,12 +45,57 @@ Template.guest.rendered = function () {
 };
 
 Template.guest.events({
+  /*
   'click .js-routine-select': function (e) {
-    e.preventDefault();
+    //e.preventDefault();
     var id = $(e.target).data('id');
-    console.log(id);
+    var input = $(e.target).children(':radio').attr('checked', true);
+    //console.log('Routine btn clicked. Id: ' + id);
     //Router.go('calendar', {view: 'monthly', id: id});
   },
+  */
+  'click .js-routine-select': function (e) {
+    e.preventDefault();
+    //$('[contenteditable=true]').attr('contenteditable', false);
+    if ($(e.target).hasClass('active')) {
+      $(e.target).attr('contenteditable','true');
+      $(e.target).focus().select();
+      $('[contenteditable=true]').not($(e.target)).attr('contenteditable', false);
+    }
+    var id = $(e.target).data('id');
+    Session.set('routineId', id);
+  },
+  'keydown [contenteditable]': function (e) {
+    if (e.keyCode === 13) {
+      $(e.target).blur();
+      return false;
+    }
+  },
+  'mouseenter .js-routine-select': function (e) {
+    //if ($(e.target).attr('contenteditable', true))
+      //$(e.target).tooltip('show');
+  },
+  'mouseleave [contenteditable]': function (e) {
+
+    //$(e.target).tooltip('hide');
+  },
+  /*
+  window.onload = function() {
+            var div = document.getElementById('editable');
+            div.onclick = function(e) {
+                this.contentEditable = true;
+                this.focus();
+                this.style.backgroundColor = '#E0E0E0';
+                this.style.border = '1px dotted black';
+            }
+
+            div.onmouseout = function() {
+                this.style.backgroundColor = '#ffffff';
+                this.style.border = '';
+                this.contentEditable = false;
+            }
+        }
+        */
   'click .js-add': function (e) {
     swal({
       title: 'Easy Tiger!',
